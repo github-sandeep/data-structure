b1b44c7e94a87ca97f081401450d886f78877847
Sandeep Srivastava <sandeep.srivastava@myntra.com>
2021-02-01 19:49:06 +0530

    adding pRemoveDuplicates

---
+++ linked-lists/src/main/java/com/sandeep/com/sandeep/util/LinkedList.java
@@ -0,0 +1,40 @@
+package com.sandeep.com.sandeep.util;
+
+import com.sandeep.entity.LinkedListNode;
+
+public class LinkedList<T> {
+    private LinkedListNode<T> head = null;
+
+    public LinkedListNode<T> insert(LinkedListNode<T> node) {
+        if(head == null) {
+            head = node;
+        } else {
+            node.setNext(head);
+            head = node;
+        }
+        return head;
+    }
+
+    public LinkedListNode<T> remove() {
+        if(head != null) {
+            if(head.getNext() != null) {
+                head = head.getNext();
+            } else {
+                head = null;
+            }
+        }
+        return head;
+    }
+
+    public LinkedListNode<T> getHead() {
+        return head;
+    }
+
+    public void printList() {
+        LinkedListNode<T> curr = head;
+        while(curr != null) {
+            System.out.print(curr.getData() + "  ");
+            curr = curr.getNext();
+        }
+    }
+}
---
+++ linked-lists/src/main/java/com/sandeep/problem/RemoveDuplicates.java
@@ -0,0 +1,49 @@
+package com.sandeep.problem;
+
+import com.sandeep.com.sandeep.util.LinkedList;
+import com.sandeep.entity.LinkedListNode;
+
+import java.util.Hashtable;
+
+public class RemoveDuplicates {
+    public static void removeDuplicate(LinkedListNode<Integer> head) {
+        Hashtable<Integer, Boolean> set = new Hashtable();
+        LinkedListNode<Integer> curr = head;
+        LinkedListNode<Integer> prev = head;
+        while(curr != null) {
+            if(set.containsKey(curr.getData())) {
+                prev.setNext(curr.getNext());
+            } else {
+                set.put(curr.getData(), true);
+                prev = curr;
+            }
+            curr = curr.getNext();
+        }
+    }
+
+    public static void main(String args[]) {
+        LinkedList<Integer> list = new LinkedList();
+        list.insert(new LinkedListNode<Integer>(2));
+        list.insert(new LinkedListNode<Integer>(3));
+        list.insert(new LinkedListNode<Integer>(2));
+        list.insert(new LinkedListNode<Integer>(4));
+        list.insert(new LinkedListNode<Integer>(5));
+        list.insert(new LinkedListNode<Integer>(3));
+        list.insert(new LinkedListNode<Integer>(5));
+        System.out.println("List before duplicate removal: ");
+        list.printList();
+        removeDuplicate(list.getHead());
+        System.out.println("\nList after duplicate removal: ");
+        list.printList();
+
+        /*LinkedList<Integer> list = new LinkedList();
+        list.insert(new LinkedListNode<Integer>(2));
+        list.insert(new LinkedListNode<Integer>(2));
+        list.insert(new LinkedListNode<Integer>(2));
+        System.out.println("List before duplicate removal: ");
+        list.printList();
+        removeDuplicate(list.getHead());
+        System.out.println("\nList after duplicate removal: ");
+        list.printList();*/
+    }
+}
